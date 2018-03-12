acb3bf8fed176efa606f1001c5ed31546027169a
Evgen <ko-e@bk.ru>
2018-03-12 20:03:46 +0700

    Страница 168

--- page/urls.py
+++ page/urls.py
@@ -1,8 +1,15 @@
 from django.urls import  path
-from page import views
+#from page import views
+from page.twviews import GoodListView, GoodDetailView
 
 urlpatterns = [
-    path("", views.index, name = "index"),
-    path("<int:cat_id>/", views.index, name = "index"),
-    path("good/<int:good_id>/", views.good, name = "good"),
+    path("", GoodListView.as_view(), name = "index"),
+    path("<int:cat_id>/", GoodListView.as_view(), name = "index"),
+    path("good/<int:good_id>/", GoodDetailView.as_view(), name = "good"),
 ]
+"""
+urlpatterns = [
+    re_path(r'^/$', GoodListView.as_view(), name = "index"),
+    re_path(r'^(?:(?P<int:cat_id>\d+)/)?$', GoodListView.as_view(), name = "index"),
+    re_path(r'^good/(?P<int:good_id>\d+)/$', GoodDetailView.as_view(), name = "good"),
+]"""
