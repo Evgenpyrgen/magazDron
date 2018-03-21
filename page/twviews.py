1faf9df934c540ecb348677b33d4de62866b16a5
Evgen <ko-e@bk.ru>
2018-03-21 22:06:11 +0700

    Классы контроллеры стр 183

--- page/twviews.py
+++ page/twviews.py
@@ -1,13 +1,19 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Category, Good
 from django.core.paginator import Paginator, InvalidPage
+from django.views.generic.base import ContextMixin
+from .models import Category, Good
 
-class GoodListView(ListView):
+class CategoryListMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListMixin, self).get_context_data(**kwargs)
+        context["cats"] = Category.objects.order_by("name")
+        return context
+
+class GoodListView(ListView, CategoryListMixin):
     template_name = "page/index.html"
-    paginate_by = 10
+    paginate_by = 3
     cat = None
-    paginate_by = 4
     
     def get(self, request, *args, **kwargs):
         if self.kwargs["cat_id"]  == None:
@@ -18,24 +24,21 @@ class GoodListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(GoodListView, self).get_context_data(**kwargs)
-        context["cats"] = Category.objects.order_by("name")
         context["category"] = self.cat
         return context
 
     def get_queryset(self):
         return Good.objects.filter(category = self.cat).order_by("name")
 
-class GoodDetailView(DetailView):
+class GoodDetailView(DetailView, CategoryListMixin):
     template_name = "page/good.html"
     model = Good
     pk_url_kwarg = "good_id"
     
-
     def get_context_data(self, **kwargs):
         context = super(GoodDetailView, self).get_context_data(**kwargs)
         try:
             context["pn"] = self.request.GET["page"]
         except KeyError:
             context["pn"] = "1"
-        context["cats"] = Category.objects.order_by("name")
         return context
